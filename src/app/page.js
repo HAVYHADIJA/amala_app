ed075c08fdc7539ba397aa5c32cdca921cc952c0
Havy <hnakalyowa29@gmail.com>
2024-11-10 17:06:06 +0300

    added info about our chef

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,8 @@ export default function Home() {
       <h1><strong>WELCOME TO AMALA FOODS</strong></h1>
       <p>This is a nice website</p>
       <p>Make your orders today!</p>
+      <h2>MEET CHEF HAVY</h2>
+      <p>Our main baker from Harvard Culinary School</p>
     </div>
   )
 }
